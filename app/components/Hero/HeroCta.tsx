304838f60ed28148ef681ba75915fe4eb605a599
sizan14789 <sizanalt@gmail.com>
2026-07-26 15:25:03 +0600

    Hero CTA Project button navigation added

--- app/components/Hero/HeroCta.tsx
+++ app/components/Hero/HeroCta.tsx
@@ -5,9 +5,17 @@ import { getContext } from "@/context/AppContext";
 export default function HeroCta() {
   const { projectRef } = getContext();
 
+  const scrollToProject = () => {
+    projectRef?.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="mt-6 flex gap-4 justify-center md:justify-start">
-      <button className="button-primary ">Projects</button>
+      <button className="button-primary " onClick={scrollToProject}>
+        Projects
+      </button>
       <a href="/assets/resume.pdf" target="_blank" rel="noopener noreferrer">
         <button className="button-secondary">Resume</button>
       </a>
