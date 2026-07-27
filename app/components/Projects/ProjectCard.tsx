c282eb517827d881647d6dd5a0a7bde58bb5b2f1
sizan14789 <sizanalt@gmail.com>
2026-07-27 15:20:54 +0600

    Zoomed out images in product card for mobile

--- app/components/Projects/ProjectCard.tsx
+++ app/components/Projects/ProjectCard.tsx
@@ -34,7 +34,7 @@ export default function ProjectCard({
 
   return (
     <article className="group flex-1 overflow-hidden max-w-140 card-border shadow-(--shadow)">
-      <div className="aspect-16/10 overflow-hidden bg-(--surface-hover) p-12 flex justify-center items-center">
+      <div className="aspect-16/10 overflow-hidden bg-(--surface-hover) p-8 md:p-12 flex justify-center items-center">
         <figure className="relative rounded-2xl overflow-hidden w-full h-full border border-(--white)">
           <Image
             src={image}
